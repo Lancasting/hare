8703a67ead19e0526e6d19578403a8b50a4190a1
Samuel Lancaster <samuellancaster66@gmail.com>
2022-09-24 16:28:15 -0600

    small changes working on jquery portion

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-$(document.ready (function () {
+$(document.ready(function () {
     function init() {
-}
-
-$(".btn.btn-primary").on('click' , 'li', function () {
-    prevent.default();
-    $(".form-control").empty();
-    romanize($(this).text());
-
-
-function romanize(num) {
-        if (isNaN(num))
-            return "Please enter a valid Number";
-        var digits = String(+num).split(""),
-            key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
-                "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
-                "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-            roman = "",
-            i = 3;
-        while (i--)
-            roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-        return console.log(Array(+digits.join("") + 1).join("M") + roman);
     }
 
-   
+    $(".button.btn.btn-primary").submit(function (event) {
+        console.log("button clicked");
+        let num = getElementById("numberInput")
+        romanize(num);
+
+
+
+        function romanize(num) {
+            console.log("function is running");
+            if (isNaN(num))
+                return "Please enter a valid Number";
+            var digits = String(+num).split(""),
+                key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+                    "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+                    "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+                roman = "",
+                i = 3;
+            while (i--)
+                roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+            return console.log(Array(+digits.join("") + 1).join("M") + roman);
+        }
+
+        preventDefault();
     })
     init();
 }));
