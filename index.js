902e583f3fbd6f9fe84ad81c9ab0035f823ee966
Samuel Lancaster <samuellancaster66@gmail.com>
2022-09-25 11:39:36 -0600

    cleaning up code and adding some light styling

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
     function init() {
-        console.log("JS has loaded");
+
     }
     $(".btn").click(function () {
-        console.log("button clicked");
         let num = $("#numberInput").val();
-        
-        // let num = document.querySelector("form-control");
-        // console.log(num);
         romanize(num);
 
         function romanize(num) {
-            console.log(num);
-            console.log("function is running");
             if (isNaN(num))
-                alert( "Please enter a valid Number");
+                alert("Please enter a valid Number");
             var digits = String(+num).split(""),
                 key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
                     "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
@@ -23,10 +17,9 @@ $(document).ready(function() {
                 i = 3;
             while (i--)
                 roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-                let result = (Array(+digits.join("") + 1).join("M") + roman)
-                console.log(result);
-             $(".result").text(result);
-             event.preventDefault();
+            let result = (Array(+digits.join("") + 1).join("M") + roman)
+            $(".result").text(result);
+            event.preventDefault();
         }
     })
     init();
